fb0104c1151373f2b5769aba10cef2eb31396c44
rakabawi <ra2035@hw.ac.uk>
2023-02-24 01:14:10 +0400

    authentication

--- getPatientReports-ID/getPatientReports-ID.js
+++ getPatientReports-ID/getPatientReports-ID.js
@@ -2,6 +2,7 @@ const server = require('express');
 const bodyParser = require('body-parser');
 // const cors = require('cors');
 require('dotenv').config();
+const jwt_token = require('jsonwebtoken')
 
 const port = process.env.SERVERPORT || 3050;
 const app = server();
@@ -9,39 +10,48 @@ app.use(bodyParser.json({limit:'100mb'}));
 app.use(bodyParser.urlencoded({limit:'100mb', extended:true}));
 
 const {MongoClient, ObjectId} = require('mongodb');
+const { JsonWebTokenError } = require('jsonwebtoken');
 const uri = "mongodb+srv://makram:makram@cluster0.uhuavyj.mongodb.net/"; //database server URI
 
-MongoClient.connect(uri) //connect to the server
-  .then(client => {
-    const db = client.db('hospitaldb'); //select the datbase
+const client = MongoClient.connect(uri); //connect to the server
+const dba = client.db('hospitaldb'); //select the datbase
 
-    app.get('/patient/reports-id/', (req, res)=>{    
-      
-      //get patient ID from the API request
-      const pat_id = req.query.id.toString();
 
-      //contruct the query in JSON
-      const query = [
-        {
-          '$match': {
-            '_id': new ObjectId(pat_id)
-          }
-        }, {
-          '$lookup': {
-            'from': 'report', 
-            'localField': '_id', 
-            'foreignField': 'patient_id', 
-            'as': 'report(s)'
-          }
-        }
-      ]
-      db.collection('patient').aggregate(query).toArray() //query the databese using MongoDB aggregation
-       .then(results => {
-          res.send(results); // sending the response
-        })
-        .catch(error => res.send(error))
-  });
-    
-})
+function getRole(username){
+
+  const role = dba.collection('acl').find({'username':username})
+  console.log(role)
+
+}
+
+app.get('/patient/reports-id/', (req, res)=>{    
+  
+  //get patient ID from the API request
+  const pat_id = req.query.id.toString();
+  const token = req.query.token;
+  const us = jwt_token.verify(token,'lymar');
+  console.log(us);
+
+  //contruct the query in JSON
+  const query = [
+    {
+      '$match': {
+        '_id': new ObjectId(pat_id)
+      }
+    }, {
+      '$lookup': {
+        'from': 'report', 
+        'localField': '_id', 
+        'foreignField': 'patient_id', 
+        'as': 'report(s)'
+      }
+    }
+  ]
+  dba.collection('patient').aggregate(query).toArray() //query the databese using MongoDB aggregation
+    .then(results => {
+      res.send(results); // sending the response
+    })
+    .catch(error => res.send(error))
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}...`));
