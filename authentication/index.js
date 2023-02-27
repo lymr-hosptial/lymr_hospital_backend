65287a252f07187f6713979aa037f3bc762c283f
lavanyaseetharaman <lavan.dec20@gmail.com>
2023-02-27 16:03:03 +0400

    code cleanup , structured folders

--- authentication/index.js
+++ authentication/index.js
@@ -1,106 +1,47 @@
 const server = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const { MongoClient } = require("mongodb");
+const filesystem = require("fs");
+const path = require("path");
 const jwt_token = require("jsonwebtoken");
 const dotenv = require("dotenv");
 const authRoutes = require("./routes/authroutes");
 const port = process.env.SERVERPORT || 3000;
-const uri = "mongodb+srv://makram:makram@cluster0.uhuavyj.mongodb.net/";
-
+const BASE_URL = `/api/${process.env.VERSION || "v1"}`;
 dotenv.config();
-
 const app = server();
 app.use(bodyParser.json({ limit: "100mb" }));
 app.use(bodyParser.urlencoded({ limit: "100mb", extended: true }));
 app.use(server.json(), cors());
-let db = "";
-MongoClient.connect(uri).then((client) => {
-  db = client.db("hospitaldb");
-});
-function authToken(req, res, next) {
-  //get auth token header value
-  const bearerToken = req.headers["authorization"];
-  if (typeof bearerToken !== "undefined") {
-    const bearer = bearerToken.split(" ");
-    console.log(bearer);
-    const token = bearer[1];
-    req.token = token;
-    next();//nodejs 
-  } else {
-    res.sendStatus(403);
-  }
-}
-app.get("/auth", (req, res) => res.send("lymr authentication, API!"));
-app.post("/api/login", (req, res) => {
-  let { username, password } = req.body;
-  db.collection("employee")
-    .find({ username: username, password:password })
-    .toArray()
-    .then((results) => {
-      console.log(results);
-      //res.json(results); // sending the data in json format
-      if (results.length != 0) {
-        jwt_token.sign({username:username},'lymar',(err, token)=>{
-          res.json({username,"user_id":results[0]._id,token,"message":"logged in successfully"});
-        });
-      }else{
-        let userNotExist = {
-          message : "User not exist in our system .contact the administrator"
-        }
-        res.json(userNotExist); //sending user doesn't exist
-      }
-    })
-    .catch((error) => console.error(error));
-});
-app.post("/api/newemployee", authToken, (req, res) => {
-  jwt_token.verify(req.token, 'lymar', (err, decoded) => {
-    if (!err) {
-      let {
-        first_name,
-        last_name,
-        email,
-        gender,
-        phone,
-        address,
-        username,
-        password,
-        date_of_birth,
-        date_of_join,
-        role,
-        position,
-        active,
-      } = req.body;
-      // console.log(req.body);
-      let query = {
-        "first_name" :first_name,
-        "last_name" :last_name,
-        "email":email,
-        "gender":gender,
-        "phone":phone,
-        "address":address,
-        "username":username,
-        "password":password,
-        "date_of_birth":new Date(date_of_birth),
-        "date_of_join":new Date(date_of_join),
-        "role":role,
-        "position":position,
-        "active":active
-      }
-      console.log(query);
-      db.collection('employee').insertOne(query)
-      .then(results => {
-      // console.log(results)
-        res.send(results);
-      })
-      .catch(error => res.send(error)); 
-    }else{
-      res.sendStatus(403);
-    } 
-  }); 
+//CORS-HEADERS- Required for cross origin and cross server communication
+app.use((req, res, next) => {
+  res.setHeader("Access-Control_Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,Authorization"
+  );
 
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET",
+    "POST",
+    "PATCH",
+    "DELETE",
+    "OPTIONS"
+  );
+  next();
 });
+app.get("/", (req, res) => res.send("lymr authentication, API!"));
 
+
+filesystem.readdir(path.join(__dirname, "routes"), (err, files) => {
+  if (err) console.error("error in index readdir" + err);
+
+  files.forEach((file) => {
+    console.log("LYMR Authentication index");
+    app.use(BASE_URL, require(`./routes/${file}`));
+  });
+});
 app.listen(port, () =>
   console.log(`Authentication API listening on port ${port}!`)
 );
--- authentication/routes/authroutes.js
+++ authentication/routes/authroutes.js
@@ -1,12 +1,11 @@
-const { Router } = require('express');
-const b_crypt = require('bcryptjs');
-const jwt_instance = require('jsonwebtoken');
-//import models
-const router = Router();
+const express = require("express");
+const router = express.Router();
+const {authToken} = require('../middleware/auth');
 const authController = require('../controllers/authcontrollers');
 
-router.get('/signup', authController.signup_post);
 
-router.post('/login', authController.login_post);
+router.route('/login').post(authController.login);
+
+router.route('/addemployee').post(authToken,authController.addEmployee);
 
 module.exports = router;
