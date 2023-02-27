7aad803c8ef7be08a798648ec5a4d5cd6c300def
Makram <mm2133@hw.ac.uk>
2023-02-28 01:28:52 +0400

    code structure

--- registration/index.js
+++ registration/index.js
@@ -1,116 +1,48 @@
-const server = require('express');
-const bodyParser = require('body-parser');
-require('dotenv').config({path: '../.env'});
-const jwt_token = require('jsonwebtoken')
-
-const port = process.env.SERVERPORT;
-const key = process.env.KEY;
+const server = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const filesystem = require("fs");
+const path = require("path");
+const jwt_token = require("jsonwebtoken");
+const dotenv = require("dotenv");
+const authRoutes = require("./routes/regroutes");
+const port = process.env.SERVERPORT || 3000;
+const BASE_URL = `/api/${process.env.VERSION || "v1"}`;
+dotenv.config();
 const app = server();
-app.use(bodyParser.json({limit:'100mb'}));
-app.use(bodyParser.urlencoded({limit:'100mb', extended:true}));
+app.use(bodyParser.json({ limit: "100mb" }));
+app.use(bodyParser.urlencoded({ limit: "100mb", extended: true }));
+app.use(server.json(), cors());
+//CORS-HEADERS- Required for cross origin and cross server communication
+app.use((req, res, next) => {
+  res.setHeader("Access-Control_Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,Authorization"
+  );
 
-const {MongoClient} = require('mongodb');
-const { JsonWebTokenError } = require('jsonwebtoken');
-const uri = "mongodb+srv://makram:makram@cluster0.uhuavyj.mongodb.net/"; //database server URI
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET",
+    "POST",
+    "PATCH",
+    "DELETE",
+    "OPTIONS"
+  );
+  next();
+});
+app.get("/", (req, res) => res.send("lymr authentication, API!"));
 
-MongoClient.connect(uri) //connect to the server
-  .then(client => {
-    const dba = client.db('hospitaldb'); //select the datbase
-    
 
-  app.post('/register/patient', async function(req, res){
-    //get values and token from API request
-    const first_name =req.query.fn.toString();
-    const last_name =req.query.ln.toString();  
-    const phone =req.query.ph.toString();  
-    const gender =req.query.ge.toString();  
-    const address =req.query.ad.toString();  
-    const bloodtype =req.query.bt.toString();    
-    const status =req.query.st.toString();  
-    const email =req.query.em.toString();  
-    const date_of_birth = new Date(req.query.dob);  
-    
-    const token = req.query.token;
-    const us = jwt_token.verify(token,key); //verify the token and get the username
+filesystem.readdir(path.join(__dirname, "routes"), (err, files) => {
+  if (err) console.error("error in index readdir" + err);
 
-    const arole = await dba.collection('employee').find({'username':us.username}).toArray();    //get the role from the employee collection in the DB
-    const access = await dba.collection('acl').find({'role':arole[0].role}).toArray();      //get the access right from the acl collection in the DB
-    const roleAccess= access[0].access;
-
-    if (!roleAccess.includes('registration')) {     //check if the username is authorized to register new employees
-      res.json({'Error':'User not authorized'})   //User is not authorized
-    } else {
-    //construct query in JSON
-    const query ={ 
-      "first_name": first_name,
-      "last_name" : last_name,
-      "email": email,
-      "gender": gender,
-      "phone": phone,
-      "address": address,
-      "date_of_birth" : date_of_birth,
-      "status": status,
-      "bloodtype": bloodtype}
-      console.log(query);
-
-    dba.collection('patient').insertOne(query)  //inserting the JSON file in the collection 'patient'
-      .then(results => {
-        res.send(results);
-      })
-      .catch(error => console.error(error))
-    }
+  files.forEach((file) => {
+    console.log("LYMR Authentication index");
+    app.use(BASE_URL, require(`./routes/${file}`));
   });
+});
+app.listen(port, () =>
+  console.log(`Authentication API listening on port ${port}!`)
+);
 
-  app.post('/register/employee/', async function(req, res){    
-      
-    //get values and token from the API request
-    const email = req.query.em.toString();
-    const first_name = req.query.fn.toString();
-    const last_name = req.query.ln.toString();
-    const gender = req.query.ge.toString();
-    const phone = req.query.ph.toString();
-    const addr = req.query.ad.toString();
-    const username = req.query.un.toString();
-    const password = req.query.pw.toString();
-    const dob = new Date(req.query.dob);
-    const doj = new Date (req.query.doj);
-    const role = req.query.role.toString();
-    const pos = req.query.pos.toString();
-
-    const token = req.query.token;
-    const us = jwt_token.verify(token,key); //verify the token and get the username
-
-    const arole = await dba.collection('employee').find({'username':us.username}).toArray();    //get the role from the employee collection in the DB
-    const access = await dba.collection('acl').find({'role':arole[0].role}).toArray();      //get the access right from the acl collection in the DB
-    const roleAccess= access[0].access;
-
-    if (!roleAccess.includes('registration')) {     //check if the username is authorized to register new employees
-      res.json({'Error':'User not authorised'})   //User is not authorized
-    }else {
-        //contruct the query in JSON
-        const query = {
-          "email": email, 
-          "first_name": first_name,
-          "last_name": last_name,
-          "gender": gender,
-          "phone": phone,
-          "address": addr,
-          "username": username,
-          "password": password,
-          "date_of_birth": dob,
-          "date_of_join": doj,
-          "role": role,
-          "position": pos,
-          "active":true
-        }
-        dba.collection('employee').insertOne(query) //inserting the JSON file in the collection 'employee'
-        .then(results => {
-            res.send(results); // sending the respone
-          })
-          .catch(error => res.send(error))
-      }
-  });
-  
-})
-
-app.listen(port, () => console.log(`Listening on port ${port}...`));
--- registration/routes/regroutes.js
+++ registration/routes/regroutes.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const {authToken} = require('../middleware/auth');
-const authController = require('../controllers/authcontrollers');
+const regController = require('../controllers/regcontrollers');
 
 
-router.route('/login').post(authController.login);
-
-router.route('/addemployee').post(authToken,authController.addEmployee);
-router.route('/addpatient').post(authToken,authController.addPatient);
+router.route('/registeremployee').post(authToken,regController.registerEmployee);
+router.route('/registerpatient').post(authToken,regController.registerPatient);
 module.exports = router;
