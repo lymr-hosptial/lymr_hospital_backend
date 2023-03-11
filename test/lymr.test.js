3c88dd934ba3d1934cb5d589d915cdc3c73ef486
rakabawi <ra2035@hw.ac.uk>
2023-03-11 12:52:08 +0400

    Testing

---
+++ test/lymr.test.js
@@ -0,0 +1,17 @@
+const { assert } = require("chai");
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+let server = require("index");
+
+chai.should();
+chai.use(chaiHttp);
+
+function helloworld() {
+    return "Hello world!";
+  }
+  
+  describe("test / route", () => {
+    it("should", function () {
+      chai.assert.equal(helloworld(), "Hello world!");
+    });
+  });
